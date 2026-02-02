c5c0761679c80f49e57ca59d3f554854972ae4a9
Оля <kovinko_olga@mail.ru>
2026-02-02 17:06:14 +0300

    v1.0

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -2,12 +2,11 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAppTheme } from "../theme/ThemeProvider";
 
-export default function Screen({ children, style }) {
+export default function Screen({ children, style, edges = ["left", "right", "bottom"] }) {
   const { colors } = useAppTheme();
   return (
-    <SafeAreaView style={[{ flex: 1, backgroundColor: colors.bg }, style]} edges={["top", "left", "right"]}>
+    <SafeAreaView style={[{ flex: 1, backgroundColor: colors.bg }, style]} edges={edges}>
       {children}
     </SafeAreaView>
   );
 }
-
